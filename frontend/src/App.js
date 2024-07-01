dc8541e9d836ae8968de6807c734136a332ff667
Hamza-Ali <hamza.919p@gmail.com>
2024-07-01 18:13:03 +0200

    built header using react

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      
+        <Nav  />
+
+        <Routes>
+          <Route path='/' element={ <h1>In Home</h1> }/>
+          <Route path='/add' element={ <h1>In Add</h1> } />
+          <Route path='/update' element={<h1>In Update</h1>} />
+          <Route path='/logout' element={<h1>In Logout</h1>} />
+          <Route path='/profile' element={<h1>In profile</h1>} />
+        </Routes>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
